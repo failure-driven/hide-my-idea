831990747d5f0eef9add97fb59f3f73d4f7ae6b4
Michael Milewski <saramic@gmail.com>
2020-06-18 07:44:50 +1000

    basic add idea and mix in product hunt ideas
    Next
    - [ ] styling
    - [ ] drag and drop re-ordering
    - [ ] idea inline - editing
    - [ ] send to friend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [term, setTerm] = useState("");
+  const [name, setName] = useState("");
+  const [tagline, setTagline] = useState("");
+  const [foundIdeas, setFoundIdeas] = useState([]);
+  const [ideas, setIdeas] = useState([
+    {
+      id: 0,
+      name: "demo name",
+      tagline: "demo tagline",
+    },
+  ]);
+  const fetchIdeas = (event) => {
+    setTerm(event.target.value);
+    // freely available API and key from
+    // https://github.com/producthunt/producthunt-api/wiki/Product-Hunt-APIs
+    fetch(
+      `https://0h4smabbsg-dsn.algolia.net/1/indexes/Post_production?query=${event.target.value}`,
+      {
+        method: "GET",
+        headers: {
+          "X-Algolia-API-Key": "9670d2d619b9d07859448d7628eea5f3",
+          "X-Algolia-Application-Id": "0H4SMABBSG",
+          "Content-type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        }
+        return response.json().then((error) => {
+          // this.setState({ error });
+          throw new Error(`Request rejected with status ${response.status}`);
+        });
+      })
+      .then((response) =>
+        response.json().then((data) => {
+          const nameAndTagline = data.hits.map(({ name, tagline }) => {
+            return {
+              name,
+              tagline,
+            };
+          });
+
+          setFoundIdeas(nameAndTagline);
+        })
+      );
+  };
+  const appendIdea = (idea) => {
+    setIdeas([
+      ...ideas,
+      {
+        id: ideas.length + 1,
+        ...idea,
+      },
+    ]);
+    setName("");
+    setTagline("");
+  };
+  const removeIdea = (id) => {
+    setIdeas(ideas.filter((idea) => idea.id !== id));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hide my idea</h1>
       </header>
+      <form>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Tag line</label>
+          <textarea
+            value={tagline}
+            onChange={(event) => setTagline(event.target.value)}
+          ></textarea>
+        </div>
+        <input
+          type="submit"
+          value="➕"
+          onClick={(event) => {
+            event.preventDefault();
+            appendIdea({ name, tagline });
+          }}
+        />
+        <div className="form-group">
+          <label>Find ideas to hide amongst</label>
+          <input type="text" value={term} onChange={fetchIdeas}></input>
+          <input
+            type="submit"
+            value="✖︎"
+            onClick={(event) => {
+              event.preventDefault();
+              setFoundIdeas([]);
+            }}
+          />
+        </div>
+        <ul>
+          {foundIdeas.map(({ id, name, tagline }) => (
+            <li key={id}>
+              {name}
+              <br />
+              {tagline}
+              <input
+                type="submit"
+                value="➕"
+                onClick={(event) => {
+                  event.preventDefault();
+                  appendIdea({ name, tagline });
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </form>
+      <ul>
+        {ideas.map(({ id, name, tagline }) => (
+          <li key={id}>
+            {name}
+            <br />
+            {tagline}
+            <input type="submit" value="✖︎" onClick={() => removeIdea(id)} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
